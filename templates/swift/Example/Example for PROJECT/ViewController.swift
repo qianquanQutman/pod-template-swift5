6107515f19069b36b0c456b9856a2a9d216588d9
qianquan <qianquan@shouqiev.com>
2022-08-25 18:06:03 +0800

    replace template xcode version

---
+++ templates/swift/Example/Example for PROJECT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PROJECT
+//
+//  Created by PROJECT_OWNER on TODAYS_DATE.
+//  Copyright (c) PROJECT_OWNER PROJECT_OWNER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
